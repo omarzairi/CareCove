29914dd3f79efc9c0279abfc07fff74ea63c873f
omar zairi <zairiomar044@gmail.com>
2023-04-28 12:32:41 +0100

    Notification Modified

--- backend/services/NotificationService.js
+++ backend/services/NotificationService.js
@@ -2,50 +2,50 @@ import asyncHandler from "express-async-handler";
 import express from "express";
 import bcrypt from "bcryptjs";
 import res from "express/lib/response.js";
+import Notification from "../models/Notification.js";
 const notificationService = {
-   async createNotification({action, dateNotification, person}) {
-        const notification = await Notification.create({
-            action,
-            dateNotification,
-            person,
-        });
-        return await notification.save();
-    },
-    async getNotificationById(notificationId) {
-        const notification = await Notification.findById(notificationId);
-        if (!notification) {
-            res.status(404).json({ message: "Notification Not Found" });
-        }
-        return  notification;
-    },
-    async getAllNotifications() {
-        const notifications = await Notification.find();
-        return notifications.map((notification) => notification.toObject());
-    },
+  async createNotification({ action, dateNotification, person }) {
+    const notification = await Notification.create({
+      action,
+      dateNotification,
+      person,
+    });
+    return await notification.save();
+  },
+  async getNotificationById(notificationId) {
+    const notification = await Notification.findById(notificationId);
+    if (!notification) {
+      res.status(404).json({ message: "Notification Not Found" });
+    }
+    return notification;
+  },
+  async getAllNotifications() {
+    const notifications = await Notification.find();
+    return notifications.map((notification) => notification.toObject());
+  },
 
-    async updateNorification(notificationId, updateData) {
-        const notification = await Notification.findById(notificationId);
-        if (!notification) {
-            throw new Error('Notification not found');
-        }
-        notification.action = updateData.action || notification.action;
-        notification.dateNotification = updateData.dateNotification || notification.dateNotification;
-        notification.person = updateData.person || notification.person;
-        
-        const updatedNotification = await notification.save();
-        return updatedNotification.toObject();
+  async updateNorification(notificationId, updateData) {
+    const notification = await Notification.findById(notificationId);
+    if (!notification) {
+      throw new Error("Notification not found");
+    }
+    notification.action = updateData.action || notification.action;
+    notification.dateNotification =
+      updateData.dateNotification || notification.dateNotification;
+    notification.person = updateData.person || notification.person;
 
-    },
+    const updatedNotification = await notification.save();
+    return updatedNotification.toObject();
+  },
 
-    async deleteNotification(notificationId) {
-        const notification = await Notification.findById(notificationId);
-        if (!notification) {
-            throw new Error('Notification not found');
-        }
-        await Notification.findByIdAndDelete(notificationId);
-        return Notification.toObject();
-    },
-
+  async deleteNotification(notificationId) {
+    const notification = await Notification.findById(notificationId);
+    if (!notification) {
+      throw new Error("Notification not found");
+    }
+    await Notification.findByIdAndDelete(notificationId);
+    return Notification.toObject();
+  },
 };
 
 export default notificationService;
