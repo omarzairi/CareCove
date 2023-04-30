06557e1b0c066272c031899bdd531cc6943ea1e2
Azizlouati1 <azizlouati338@gmail.com>
2023-04-30 12:54:55 +0100

    creation of patient model,entite,service,controller

---
+++ backend/entities/Patient.js
@@ -0,0 +1,32 @@
+
+class PatientClass extends Person {
+  constructor(firstName, lastName, birthDate, gender, role, email, password, allergies, bloodType, height, weight) {
+    super(firstName, lastName, birthDate, gender, role, email, password);
+    this.allergies = allergies;
+    this.bloodType = bloodType;
+    this.height = height;
+    this.weight = weight;
+  }
+
+  getAllergies() { return this.allergies; }
+  getBloodType() { return this.bloodType; }
+  getHeight() { return this.height; }
+  getWeight() { return this.weight; }
+
+  setAllergies(allergies) { this.allergies = allergies; }
+  setBloodType(bloodType) { this.bloodType = bloodType; }
+  setHeight(height) { this.height = height; }
+  setWeight(weight) { this.weight = weight; }
+
+  toObject() {
+    return {
+      ...super.toObject(),
+      allergies: this.allergies,
+      bloodType: this.bloodType,
+      height: this.height,
+      weight: this.weight,
+    };
+  }
+}
+
+export default PatientClass;
---
+++ backend/services/PatientService.js
@@ -0,0 +1,50 @@
+import Patient from "../models/Patient.js";
+import Person from "../models/Person.js";
+
+const patientService = {
+  async createPatient({ person, allergies, bloodType, height, weight }) {
+    const patient = await Patient.create({
+      person,
+      allergies,
+      bloodType,
+      height,
+      weight,
+    });
+    return await patient.save();
+  },
+  async getPatientById(patientId) {
+    const patient = await Patient.findById(patientId);
+    if (!patient) {
+      throw new Error("Patient not found");
+    }
+    return patient;
+  },
+  async getAllPatients() {
+    const patients = await this.find();
+    return patients.map((patient) => patient.toObject());
+  },
+  async updatePatient(patientId, updateData) {
+    const patient = await Patient.findById(patientId).populate("person");
+    if (!patient) {
+      throw new Error("Patient not found");
+    }
+    patient.person = updateData.person || patient.person;
+    patient.allergies = updateData.allergies || patient.allergies;
+    patient.bloodType = updateData.bloodType || patient.bloodType;
+    patient.height = updateData.height || patient.height;
+    patient.weight = updateData.weight || patient.weight;
+    const updatedPatient = await patient.save();
+    return updatedPatient.toObject();
+  },
+  async deletePatient(patientId) {
+    const patient = await Patient.findById(patientId);
+    if (!patient) {
+      throw new Error("Patient not found");
+    }
+    await Person.findByIdAndDelete(patient.person);
+    await Patient.findByIdAndDelete(patientId);
+    return patient.toObject();
+  },
+};
+
+export default patientService;
