06aa621d88a9c8cf8705bc674c005b6d54e68781
- Amine - <mzoughi.mine@gmail.com>
2023-05-01 18:35:35 +0100

    doctor

--- backend/entities/Doctor.js
+++ backend/entities/Doctor.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import Person from "./Person.js";
-class DoctorClass extends Person{
-    constructor(firstName, lastName, birthDate, gender,role, email, password,location,rating,person)
-    {
-=======
 import PersonClass from "./Person.js";
 
 class DoctorClass extends PersonClass{
-    constructor(firstName, lastName, birthDate, gender,role, email, password,location,rating){
->>>>>>> 0c0a8f945fe7e55beda2368c6b01bbe689672cc4
+    constructor(firstName, lastName, birthDate, gender,role, email, password,location,rating)
+    {
         super(firstName, lastName, birthDate, gender,role, email, password);
         this.location = location;
         this.rating = rating;
